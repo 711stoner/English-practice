43557dee8067478fa776d420d3e7e1f10d3db9e2
711stoner <juejiangfm@gmail.com>
2026-03-05 18:48:54 +0800

    Use hash router to avoid server 404

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { migrateLegacyKeys } from "./storage/sentencesStore.js";
 import "./index.css";
@@ -9,8 +9,8 @@ migrateLegacyKeys();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
